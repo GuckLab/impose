5b46c31920a73ed0eb1c094fdfe9c8f8e28fbb11
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2022-03-24 12:59:44 +0100

    Add test for bmlab file with 2 repetitions

--- tests/test_fmt_h5_brillouin_bmlab.py
+++ tests/test_fmt_h5_brillouin_bmlab.py
@@ -23,6 +23,29 @@ def test_load_bmlab_brillouin():
     assert meta["signature"] == "25d6ae8774fc29417facc6c9dfe061e6"
 
 
+def test_load_bmlab_brillouin_2rep():
+    paths = retrieve_data("fmt_brillouin-h5_bmlab-session_2022_2-rep.zip")
+    data, meta = formats.load(paths[0])
+
+    # check metadata
+    assert meta["pixel size x"] == 1
+    assert meta["pixel size y"] == 1
+    assert np.isnan(meta["pixel size z"])
+    assert meta["shape"] == (3, 5, 1)
+    assert "rep-0_brillouin_peak_position" in meta["channel hues"]
+    assert "rep-1_brillouin_peak_position" in meta["channel hues"]
+
+    # check data
+    assert "rep-0_brillouin_peak_position" in data
+    assert data["rep-0_brillouin_peak_position"].shape == (3, 5, 1)
+    assert data["rep-0_brillouin_peak_position"][2, 3, 0] == 55.53730853499501
+    assert "rep-1_brillouin_peak_position" in data
+    assert data["rep-1_brillouin_peak_position"].shape == (3, 5, 1)
+    assert data["rep-1_brillouin_peak_position"][2, 3, 0] == 101.47705516241298
+
+    assert meta["signature"] == "0f9dfe4cfe549859ac09ec0ead5bf58f"
+
+
 def test_load_bmlab_brillouin_signature():
     paths = retrieve_data("fmt_brillouin-h5_bmlab-session_2022.zip")
     _, meta1 = formats.load(paths[0])
