78d23ef111fbfd5fb57f17e595c26d4fe0032bd8
Paul Müller <dev@craban.de>
2024-05-17 10:34:10 +0200

    setup: bump dependency versions

--- setup.py
+++ setup.py
@@ -29,13 +29,14 @@ setup(
     long_description=open('README.rst').read() if exists('README.rst') else '',
     install_requires=["czifile==2019.7.2",  # bc cgohlke and used for signature
                       "bmlab==0.6.1",
-                      "h5py>=2.10.0",
+                      "h5py>=3.11.0",
+                      "imageio",  # open image files
                       "numpy>=1.17.0",
                       "pyqt6>=6.2.0",
-                      "pyqtgraph==0.13.3",
+                      "pyqtgraph==0.13.7",
                       "scikit-image>=0.17.2",
                       "scipy>=0.12.0",  # compute size of polygon shape
-                      "imageio",  # open image files
+                      "tifffile==2024.5.10",
                       ],
     python_requires=">=3.9",
     keywords=["image analysis", "biology", "microscopy"],
